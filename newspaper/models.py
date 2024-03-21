c29123e9ae787ab4f37ba8a6a7b556d26af072d8
Molchanov Ihor <IMolchanov@kdmg.com.ua>
2024-03-21 13:26:11 +0200

    change visible data about publisher (redactor model)

--- newspaper/models.py
+++ newspaper/models.py
@@ -11,7 +11,7 @@ class Redactor(AbstractUser):
         verbose_name_plural = "Redactors"
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.username} ({self.first_name} {self.last_name})"
 
     def get_absolute_url(self):
         return reverse("newspaper:redactor-detail", kwargs={"pk": self.pk})
