ea063b97fbe4220bf73ff84b001d4dfcbbbd3af9
Molchanov Ihor <=>
2024-03-23 11:58:13 +0200

    add gunicorn to requirements.txt & create build.sh

--- web_config/settings.py
+++ web_config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv["DJANGO_SECRET_KEY"]
 
 DEBUG = os.getenv("DJANGO_DEBUG", "") != "False"
 ALLOWED_HOSTS = ["localhost"]
